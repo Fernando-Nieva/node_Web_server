d32210002ee470bd5093c8c90a7987ba8e03e530
Fernando Nieva <nieva.cronos@gmail.com>
2024-04-26 09:40:46 -0300

    Realizamos un servidor web de post

---
+++ src/config/env.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+const { get } = require('env-var');
+
+const envs = {
+    // PORT: get('PORT').required().asPortNumber(),
+    PORT: get('PORT').default(3000).asPortNumber(),
+
+    PUBLIC_PATH: get('PUBLIC_PATH').default('public').asString()
+};
+
+module.exports = {
+    envs
+};
+
+
